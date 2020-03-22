7081e7c4436d5144b5744373a78d03e6454be27e
hisanari <hisanari.1987@gmail.com>
2020-03-22 12:13:50 +0900

    add template

---
+++ template/main.rb
@@ -0,0 +1,8 @@
+require './plain_text_report'
+require './html_report'
+
+plain_report = PlainTextReport.new
+plain_report.output
+
+html_report = HtmlReport.new
+html_report.output
---
+++ template/plain_text_report.rb
@@ -0,0 +1,32 @@
+require './report'
+
+class PlainTextReport < Report
+  def output_start
+    puts 'Start plain text report!'
+  end
+
+  def output_head
+    puts "***** #{@title} *****"
+    puts
+  end
+
+  def output_body_start
+  end
+
+  #def output_body
+  #  @text.each do |line|
+  #    output_line(line)
+  #  end
+  #end
+  
+  def output_line(line)
+    puts line
+  end
+  
+  def output_body_end
+  end
+  
+  def output_end
+    puts 'End plain text report!'
+  end
+end
